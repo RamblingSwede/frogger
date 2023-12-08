a69da766e6a3bdffa886f96f6761bcc101cc8a56
RamblingSwede <70953558+RamblingSwede@users.noreply.github.com>
2023-12-08 13:59:13 +0000

    Add frog placeholder

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ class Game:
         pygame.init()
         pygame.display.set_caption('Frogger')
         self.screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-
+        self.frog = pygame.image.load("./resources/frog_placeholder.png").convert_alpha()
 
     def run(self):
         while True:
             self.screen.fill('Black')
+            self.screen.blit(self.frog, (SCREENWIDTH // 2,400))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+
+
             pygame.display.update()
 
 Game().run()
