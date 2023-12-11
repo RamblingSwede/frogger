92f793868966738f1a3856f97df4d6081986e407
Oliver Persson <oliver.persson.8578@student.lu.se>
2023-12-12 00:43:44 +0100

    Added list class to contain entities/floaters

---
+++ entity_list.py
@@ -0,0 +1,17 @@
+class entity_list: 
+    def __init__(self):
+        self.entities = [] 
+
+    def add(self, entity): 
+        self.entities += entity 
+
+    def add(self, entities): 
+        self.entities.extend(entities) 
+
+    def update_locations(self): 
+        for entity in self.entities: 
+            entity.update_location() 
+
+    def draw(self, screen): 
+        for entity in self.entities: 
+            entity.draw(screen) 
