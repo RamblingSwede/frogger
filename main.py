e03cf9d2e61c67b5b20e1f6e6783b9e781c804b9
Oliver Persson <oliver.persson.8578@student.lu.se>
2023-12-12 00:44:08 +0100

    Edited code to add floaters to screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from entities import *
+from entity_list import * 
 import pygame
 import sys
 
@@ -13,17 +14,26 @@ class Game:
         self.clock      = pygame.time.Clock()
         self.screen     = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT)) 
         self.background = Background(SCREENWIDTH, SCREENHEIGHT, SIZE) 
-        self.frog       = Frog(SCREENWIDTH, SCREENHEIGHT, SIZE)
+        self.frog       = Frog(SCREENWIDTH, SCREENHEIGHT, SIZE) 
+        self.floaters   = self.add_river_floaters()
         self.update_display() 
+
+    def add_river_floaters(self): 
+        log_2 = Floater(1, 128, 3, pygame.image.load("./resources/log_2_placeholder.png").convert_alpha())
+        log_3 = Floater(1, 32, 2, pygame.image.load("./resources/log_3_placeholder.png").convert_alpha())
+        log_4 = Floater(1, 96, 1, pygame.image.load("./resources/log_4_placeholder.png").convert_alpha())
+        floaters = entity_list()
+        floaters.add([log_2, log_3, log_4]) 
+        return floaters 
 
     def update_display(self): 
         self.screen.fill('Black')
         self.background.draw(self.screen) 
         self.frog.draw(self.screen) 
+        self.floaters.draw(self.screen) 
         
     def run(self):
         while True:
-            self.update_display() 
             if self.frog.rect.x >= SCREENWIDTH - SIZE:
                   self.frog.rect.x = SCREENWIDTH - SIZE
             if self.frog.rect.x <= 0:
@@ -58,6 +68,9 @@ class Game:
 
             self.frog.rect.x += self.frog.velX
             self.frog.rect.y += self.frog.velY
+            self.floaters.update_locations() 
+
+            self.update_display() 
             pygame.display.update()
             self.clock.tick(FPS)
 
